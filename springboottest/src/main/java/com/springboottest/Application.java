39085331fb2874c35e560b88d808760dcfbeaab5
Administrator <Administrator@WIN-G17SJU17FK6>
2020-11-17 10:47:45 +0800

    使用外部tomcat;以及修改tomcat跟路径

--- springboottest/src/main/java/com/springboottest/Application.java
+++ springboottest/src/main/java/com/springboottest/Application.java
@@ -2,14 +2,20 @@ package com.springboottest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan(basePackages = "com.*")
-public class Application {
+public class Application extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		SpringApplication.run(Application.class, args);
 	}
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(Application.class);
+    }
 }
